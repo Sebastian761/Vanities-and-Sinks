c0193102f92d752ff6f30d442715378a8012cb16
Sebastian Ocando <ocandovivas@gmail.com>
2023-09-26 10:25:30 -0300

    services component

--- src/components/About/Banner.jsx
+++ src/components/About/Banner.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export const Banner = () => {
     return (
         <>
-            <section className="overflow-hidden h-[580px] bg-[url(https://images.unsplash.com/photo-1595514535215-8a093483610a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)] w-full bg-cover bg-fixed bg-top bg-no-repeat">
+            <section className="overflow-hidden h-[580px] bg-[url(https://images.unsplash.com/photo-1538430224529-7eceed6ab252?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)] w-full bg-cover bg-fixed bg-bottom bg-no-repeat">
             <div className="bg-black/40 p-8 md:p-12 lg:px-16 lg:py-24 h-[1020px] lg:h-[580px] flex flex-col laptop:flex-row space-between">
                 <div className=" w-full md:w-1/2 text-center md:text-left mt-[25%] sm:mt-[15%] md:mt-0 ">
-                    <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-4xl xtablet:text-5xl">
+                    <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-4xl xltablet:text-5xl pb-3">
                         Our Services at Your Disposal
                     </h2>
 
-                    <p className="max-w-lg text-white mt-6 md:block md:text-lg leading-relaxed">
+                    <p className="m-auto md:m-0 max-w-lg text-white mt-6 md:block md:text-lg leading-relaxed">
                         At /Your Store Name/ we not only offer you the best sinks and vanities, but we also provide you with a full suite of services to make your experience exceptional. From expert sink installation to personalized bathroom design advice and fast, secure deliveries, we are here to meet your every need. Trust us to make your bathroom project a reality - find out how we can help you below!
                     </p>
 
@@ -22,8 +22,8 @@ export const Banner = () => {
                         Contact us now!
                         </Link>
                     </div>
-                </div>
-                    <img src="https://images.unsplash.com/photo-1595514535215-8a093483610a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="sink"  className="hidden 2xl:block h-[540px] w-[full] w-1/2 m-auto shadow-xl absolute top-42 right-12"/>
+                </div >
+                    <img src="https://images.unsplash.com/photo-1595428773960-5bea2830b067?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2044&q=80" alt="sink"  className="hidden 2xl:block h-[540px] w-[full] w-1/2 m-auto shadow-xl absolute opacity-90 top-42 right-12"/>
             </div>
         </section>
         </>
--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -1,33 +1,35 @@
 export const Info = () => {
     return (
         <>
-        <div className="flex flex-row mt-12 md:mt-24 h-[640px]">
-            <div className="flex flex-col lg:flex-row w-[60%] mr-20">
+        <div className="flex flex-col laptop:flex-row mt-12 2xl:mt-40 laptop:mt-12 md:h-[960px] xl:h-[920px] laptop:h-[640px]">
+            <div className="flex flex-col md:flex-row w-full gap-8">
                 <img
-                className="h-96 w-full rounded-lg object-cover object-center w-[44%] m-auto"
-                src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-                alt="nature image"
+                className="h-96 rounded-lg object-cover object-center w-[80%] md:w-[40%] m-auto shadow-xl"
+                src="https://images.unsplash.com/photo-1584069793933-57852d7060ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+                alt="bathroom"
                 />
                 <img
-                className="h-96 w-full rounded-lg object-cover object-center w-[44%] m-auto"
-                src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-                alt="nature image"
+                className="h-96 rounded-lg object-cover object-center w-[80%] md:w-[40%] m-auto shadow-xl"
+                src="https://images.unsplash.com/photo-1595515106886-43b1443a2e8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1994&q=80"
+                alt="bathroom"
                 />
             </div>
-            <div className="w-[40%] ">
-                <div className="gap-24 py-8 px-4 mx-auto 2xl lg:grid  lg:grid-cols-2 lg:py-16 lg:px-6 h-full">
-                    <div className="sm:text-lg w-96">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-darkBlue dark:text-white">
-                        Underlay Laminate Flooring Service & Wooden Floors
-                        </h2>
-                        <p className="mb-4 font-semibold text-darkBlue text-lg">
-                        We are committed to making your floor look its best and for you to
-                        be completely satisfied with our service.
-                        </p>
-                        <div className="mt-8 flex gap-8 flex-col">
+            <div className="w-full laptop:w-[50%] mt-12 laptop:mt-0 ">
+                <div className="gap-32 w-full laptop:w-auto py-8 px-4 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 ">
+                    <div className="sm:text-lg w-[80vw] lg:w-[70vw] flex flex-col md:flex-row laptop:block laptop:w-96 m-auto lg:ml-[30%] md:gap-8">
+                        <div>
+                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-darkBlue dark:text-white">
+                            Underlay Laminate Flooring Service & Wooden Floors
+                            </h2>
+                            <p className="mb-4 font-semibold text-darkBlue text-lg">
+                            We are committed to making your floor look its best and for you to
+                            be completely satisfied with our service.
+                            </p>
+                        </div>
+                        <div className="mt-8 flex gap-8 flex-col md:ml-12 laptop:ml-0">
                             <div className="flex items-center gap-5">
                             
-                                <div className=" text-gray-600">
+                                <div className=" text-gray-700">
                                     <h4 className="font-semibold text-darkBlue text-lg">
                                         FAST RELIABLE SERVICE
                                     </h4>
@@ -38,11 +40,11 @@ export const Info = () => {
                                 </div>
                                 {/* <GrPaint size={70} className="ml-2" color="" /> */}
                             </div>
-                        </div>
+                        </div> 
                         <div className="mt-8 flex gap-8 flex-col">
                             <div className="flex items-center gap-5">
                             
-                                <div className=" text-gray-600">
+                                <div className=" text-gray-700">
                                     <h4 className="font-semibold text-darkBlue text-lg">
                                         FAST RELIABLE SERVICE
                                     </h4>
